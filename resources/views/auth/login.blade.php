3d168154baacf65b457f4c69e9757a7c2413245b
Nique Alisson Pereira de Souza <alisson_201083@hotmail.com>
2022-03-04 14:38:55 -0300

    frist commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
+            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="/register">Cadastre-se</a>
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
